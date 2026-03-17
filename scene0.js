2f65f94b2d91bbee5e81a8080f8af2b509151d03
Iza-Beli <izabeli.k@aluno.ifsc.edu.br>
2026-03-17 14:21:32 +0000

    Animação e controle de personagem

--- scene0.js
+++ scene0.js
@@ -1,39 +1,120 @@
 class scene0 extends Phaser.Scene {
   constructor() {
     super("scene0");
+
+    this.threshold = 0.1;
+    this.speed = 100;
+    this.direction = undefined;
   }
+
   preload() {
     this.load.spritesheet("alien", "assets/alien.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
+
+    this.load.spritesheet("buttons", "assets/buttons.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+
+    this.load.plugin(
+      "rexvirtualjoystickplugin",
+      "./rexvirtualjoystickplugin.min.js",
+      true,
+    );
+    this.load.audio("laser", "assets/laser.mp3");
+    this.load.audio("music", "assets/music.mp3");
   }
 
   create() {
-    this.anims.create({
-      key: "stand-still",
-      frames: this.anims.generateFrameNumbers("alien", { start: 20, end: 20 }),
-      frameRate: 1
-    });
+    this.sound.add("music", { loop: true }).play();
+    this.laser = this.sound.add("laser");
 
     this.anims.create({
-      key: "walk-right",
-      frames: this.anims.generateFrameNumbers("alien", { start: 87, end: 95 }),
+      key: "walk-up",
+      frames: this.anims.generateFrameNumbers("alien", { start: 0, end: 7 }),
       frameRate: 10,
       repeat: -1,
     });
 
-    this.alien = this.physics.add
-      .sprite(400, 225, "alien", 20)
+    this.anims.create({
+      key: "walk-left",
+      frames: this.anims.generateFrameNumbers("alien", { start: 8, end: 15 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk-right",
+      frames: this.anims.generateFrameNumbers("alien", { start: 24, end: 31 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk-down",
+      frames: this.anims.generateFrameNumbers("alien", { start: 16, end: 23 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.alien = this.physics.add.sprite(400, 225, "alien", 20);
+
+    this.joystick = this.plugins.get("rexvirtualjoystickplugin").add(this, {
+      x: 100,
+      y: 350,
+      radius: 50,
+      base: this.add.circle(0, 0, 50, 0xcccccc),
+      thumb: this.add.circle(0, 0, 25, 0x666666),
+    });
+
+    this.joystick.on("update", () => {
+      const angle = Phaser.Math.DegToRad(this.joystick.angle);
+      const force = this.joystick.force;
+
+      if (force > this.threshold) {
+        this.direction = new Phaser.Math.Vector2(
+          Math.cos(angle),
+          Math.sin(angle),
+        ).normalize();
+      }
+
+      if (this.joystick.force > 0) {
+        this.alien.setVelocity(
+          this.direction.x * this.speed,
+          this.direction.y * this.speed,
+        );
+
+        switch (true) {
+          case this.joystick.angle >= -135 && this.joystick.angle < -45:
+            this.alien.anims.play("walk-up", true);
+            break;
+          case this.joystick.angle >= -45 && this.joystick.angle < 45:
+            this.alien.anims.play("walk-right", true);
+            break;
+          case this.joystick.angle >= 45 && this.joystick.angle < 135:
+            this.alien.anims.play("walk-down", true);
+            break;
+          case this.joystick.angle >= 135 || this.joystick.angle < -135:
+            this.alien.anims.play("walk-left", true);
+            break;
+        }
+      } else {
+        this.alien.setVelocity(0, 0);
+        this.alien.anims.stop();
+      }
+    });
+
+    this.button = this.add
+      .sprite(700, 350, "buttons", 10)
       .setInteractive()
       .on("pointerdown", () => {
-        if (this.alien.body.velocity.x === 0) {
-          this.alien.play("walk-right");
-          this.alien.setVelocityX(100);
-        } else {
-          this.alien.play("stand-still");
-          this.alien.setVelocityX(0);
-        }
+        this.button.setFrame(11);
+      })
+      .on("pointerup", () => {
+        this.button.setFrame(10);
+        this.laser.play();
       });
   }
 }
